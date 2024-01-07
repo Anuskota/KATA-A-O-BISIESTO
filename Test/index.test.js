9f916a979eb7bab77be47affadb31e55c242a517
Anuskota <anamangada.dev@gmail.com>
2024-01-07 21:49:01 +0100

    test passed

--- Test/index.test.js
+++ Test/index.test.js
@@ -2,21 +2,22 @@ const { expect } = require("@jest/globals");
 const leapYear = require("../leapYear");
 
 describe("Tests de validacion si un año no  es Bisiesto o no", () => {
+
     test("Test devuelve False si este año es divisible por 4", () => {
        
-        expect(leapYear(1997).tobe(false));
+        expect(leapYear(1997)).toBe(false);
     });
 
     test("Test devuelve True  si este año es divisible por 4", () => {
-        expect(leapYear(1996).tobe(true));
+        expect(leapYear(1996)).toBe(true);
     });
 
     test("Test devuelve True si este año es divisible por 400", () => {
-        expect(leapYear(1600).tobe(true));
+        expect(leapYear(1600)).toBe(true);
     });
 
     test("Test devuelve False si este año es divisible por 100, divisible por 4, pero no es divisible por 400, es", () => {
-        expect(leapYear(1997).tobe(false));
+        expect(leapYear(1800)).toBe(false);
     });
 
 });
